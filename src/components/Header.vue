144c9bf09cfb52216a73640706d53b71b1a0c743
naod7 <87893397+naod7@users.noreply.github.com>
2023-09-14 01:40:12 -0700

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,7 +20,17 @@
 <style scoped>
 
     header {
-        margin-top: 20px
+        margin-top: 20px;
+        margin-bottom: 10px;
+
+        display: flex;
+        align-items: center;
+        height: 65px;
+       /* background: rgb(124, 73, 11);*/
+        background: linear-gradient(90deg, #09083d 1.18%, #071130 100%);
+        color: white;
+        padding: 20px;
+        justify-content: space-between;
     }
 
     header h4 {
@@ -30,13 +40,13 @@
     .bar {
         width: 300px;
         height: 50px;
-        border: 3px solid bisque
+        border: 3px solid rgb(50, 50, 126)
     }
 
     .completion {
         height: 100%;
         width: 0%;
-        background-color: bisque;
+        background-color: rgb(50, 50, 126);
     }
 
 </style>
